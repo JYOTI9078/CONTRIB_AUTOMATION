f3fe566aae56ca98b5632a95af1ec65c93d27229
bisguha <biswajit.guha@capgemini.com>
2022-05-19 19:45:41 +0530

    add setup and teardown methods in CommontSteps

--- src/test/java/stepdefinitions/CommonSteps.java
+++ src/test/java/stepdefinitions/CommonSteps.java
@@ -1,8 +1,26 @@
 package stepdefinitions;
 
-import com.sogeti.automation.framework.utils.Logging;
+import com.sogeti.automation.framework.basetest.TestClass;
+import com.sogeti.automation.framework.basetest.TestContext;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
-public class CommonSteps {
-    Logging log = new Logging(CommonSteps.class.getName());
+public class CommonSteps extends TestClass {
+    TestContext testContext;
+    public CommonSteps(TestContext context) throws Exception {
+        super();
+        this.testContext = context;
+    }
 
+    @Before
+    public void testSetup(Scenario name) {
+        log.info("The Test Scenario id: " + name.getName());
+    }
+
+    @After
+    public void tearDown(Scenario name) {
+        testContext.getDriver().quit();
+        log.info("Execution status is: " + name.getStatus());
+    }
 }
