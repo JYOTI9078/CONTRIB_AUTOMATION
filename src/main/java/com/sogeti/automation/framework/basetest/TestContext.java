f47f08551e4e4d3b8212aa87a8b1cc264229cfa9
bisguha <biswajit.guha@capgemini.com>
2022-06-08 19:46:02 +0530

    refactor PageObjectManager and TestContext

--- src/main/java/com/sogeti/automation/framework/basetest/TestContext.java
+++ src/main/java/com/sogeti/automation/framework/basetest/TestContext.java
@@ -1,19 +1,22 @@
 package com.sogeti.automation.framework.basetest;
 
+import com.sogeti.automation.test.pageFactory.PageObjectManager;
 import org.openqa.selenium.WebDriver;
 
 public class TestContext {
     private PageObjectManager pageObjectManager;
     private TestClass testClass;
-    private WebDriver gDriver;
+    private WebDriver driver;
     String browser = null;
 
     public TestContext() throws Exception {
         this.testClass = new TestClass();
+        this.driver = testClass.setupEnvironment(browser);
+        pageObjectManager = new PageObjectManager(driver);
     }
 
     public WebDriver getDriver() {
-        return gDriver;
+        return driver;
     }
 
     public PageObjectManager getPageObjectManager() {
--- src/main/java/com/sogeti/automation/test/pageFactory/PageObjectManager.java
+++ src/main/java/com/sogeti/automation/test/pageFactory/PageObjectManager.java
@@ -1,11 +1,13 @@
-package com.sogeti.automation.framework.basetest;
+package com.sogeti.automation.test.pageFactory;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class PageObjectManager {
     WebDriver gDriver;
 
     public PageObjectManager(WebDriver driver) {
         this.gDriver = driver;
+        PageFactory.initElements(driver, this);
     }
 }
