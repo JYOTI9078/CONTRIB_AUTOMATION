183e495a5d8d2a7217a9cef1e4cce56f74c4a82a
bisguha <biswajit.guha@capgemini.com>
2022-06-13 13:56:40 +0530

    redefine page-factory logic

---
+++ src/main/java/com/sogeti/automation/test/pageFactory/PageClass.java
@@ -0,0 +1,17 @@
+package com.sogeti.automation.test.pageFactory;
+
+import com.sogeti.automation.framework.utils.WebUtils;
+import org.apache.logging.log4j.ThreadContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageClass extends WebUtils {
+
+    public PageClass(WebDriver driver) {
+        super(driver);
+//        wait = new WebDriverWait(driver, FrameworkConstants.MediumWait);
+        PageFactory.initElements(driver, this);
+        ThreadContext.pop();
+        ThreadContext.push(this.getClass().getSimpleName());
+    }
+}
