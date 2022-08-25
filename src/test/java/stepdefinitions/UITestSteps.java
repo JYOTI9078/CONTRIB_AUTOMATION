d23e8421a316bf26a310fa981d012e03d79dc838
bisguha <biswajit.guha@capgemini.com>
2022-08-25 14:22:39 +0530

    refactor UITestSteps constructor

--- src/test/java/stepdefinitions/UITestSteps.java
+++ src/test/java/stepdefinitions/UITestSteps.java
@@ -5,17 +5,14 @@ import com.sogeti.automation.framework.basetest.TestContext;
 import com.sogeti.automation.test.pageFactory.TestPage;
 import io.cucumber.java.en.Given;
 import org.apache.logging.log4j.ThreadContext;
-import org.testng.Assert;
-import org.testng.asserts.Assertion;
 
-public class TestSteps extends TestClass {
+public class UITestSteps extends TestClass {
 
     TestContext testContext;
     TestPage testPage;
 
-
-    public TestSteps(TestContext context) throws Exception {
-        super();
+    public UITestSteps(TestContext context) throws Exception {
+//        super();
         this.testContext = context;
         testPage = testContext.getPageObjectManager().getTestPage();
         ThreadContext.pop();
