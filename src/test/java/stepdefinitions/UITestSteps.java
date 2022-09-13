c27c80856c2f5b4ad86341ed87127c30e615c494
Gitea <gitea@fake.local>
2022-09-13 22:34:42 +0530

    add new test scenarios and step-definitions for SwagLabs demo

--- src/test/java/stepdefinitions/UITestSteps.java
+++ src/test/java/stepdefinitions/UITestSteps.java
@@ -2,25 +2,55 @@ package stepdefinitions;
 
 import com.sogeti.automation.framework.basetest.TestClass;
 import com.sogeti.automation.framework.basetest.TestContext;
-import com.sogeti.automation.test.pageFactory.TestPage;
+import com.sogeti.automation.framework.constants.AppConstants.Web;
+import com.sogeti.automation.test.pageFactory.SwagLabsDemo_LoginPage;
+import com.sogeti.automation.test.pageFactory.SwagLabsDemo_ProductsPage;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.apache.logging.log4j.ThreadContext;
+import org.testng.Assert;
 
 public class UITestSteps extends TestClass {
 
     TestContext testContext;
-    TestPage testPage;
+    SwagLabsDemo_LoginPage swagLabsDemoLoginPage;
+    SwagLabsDemo_ProductsPage swagLabsDemo_productsPage;
 
     public UITestSteps(TestContext context) throws Exception {
 //        super();
         this.testContext = context;
-        testPage = testContext.getPageObjectManager().getTestPage();
+        swagLabsDemoLoginPage = testContext.getPageObjectManager().getSwagLabsDemo_LoginPage();
+        swagLabsDemo_productsPage = testContext.getPageObjectManager().getSwagLabsDemo_ProductsPage();
         ThreadContext.pop();
         ThreadContext.push(this.getClass().getSimpleName());
     }
 
-    @Given("^user opens Konakart application in browser$")
+    /*@Given("^user opens Konakart application in browser$")
     public void openGoogle() throws InterruptedException {
         Thread.sleep(5000);
+    }*/
+
+    @Given("^user logs into SwagLabs portal$")
+    public void swagLabsLogin() {
+        swagLabsDemoLoginPage.login(Web.UI_USERNAME, Web.UI_PASSWORD);
+        Assert.assertTrue(swagLabsDemo_productsPage.isProductsTitleVisible(),
+                "Products page did not load. LOGIN FAILED");
+    }
+
+    @When("^user is on Products page$")
+    public void isProductsPageLoaded() {
+        Assert.assertTrue(swagLabsDemo_productsPage.isProductsTitleVisible(),
+                "Products page did not load.");
+    }
+
+    @Then("^verify that price of (.*) is (.*)")
+    public void verifyProductPrice(String productName, String productPrice) {
+        String actualProductName = swagLabsDemo_productsPage.getProductName(productName);
+        String actualProductPrice = swagLabsDemo_productsPage.getProductPrice(productName);
+
+        Assert.assertEquals(actualProductName, productName, "Actual Product Name does not match expected value.");
+        Assert.assertEquals(actualProductPrice, productPrice,
+                "Price of product '" + productName + "' does not match expected value.");
     }
 }
--- src/test/resources/Features/WebUITests/UITests.feature
+++ src/test/resources/Features/WebUITests/UITests.feature
@@ -1,5 +1,17 @@
-@tests
-Feature: Konakart application features
+@uitests
+Feature: UI Tests - Demo Feature
 
-  Scenario: Login to Konakart
-    Given user opens Konakart application in browser
+  Background:
+    Given user logs into SwagLabs portal
+
+
+  Scenario Outline: Verify the price of item
+    When user is on Products page
+    Then verify that price of <Product> is <Price>
+    Examples:
+      | Product                   | Price    |
+      | Sauce Labs Bolt T-Shirt   | $15.99   |
+      | Sauce Labs Fleece Jacket  | $49.99   |
+      | Sauce Labs Backpack       | $50.00   |
+      | Sauce Labs Bike Light     | $9.99    |
+      | Sauce Labs Bolt Jeans     | $15.99   |
