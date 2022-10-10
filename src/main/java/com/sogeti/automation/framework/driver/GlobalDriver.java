48385da296daaded90a0ec539f24885aa122d615
bisguha <biswajit.guha@capgemini.com>
2022-10-10 18:23:06 +0530

    add safari driver logic and capabilities in GlobalDriver

--- src/main/java/com/sogeti/automation/framework/driver/GlobalDriver.java
+++ src/main/java/com/sogeti/automation/framework/driver/GlobalDriver.java
@@ -18,6 +18,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.grid.Main;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.events.WebDriverListener;
 
@@ -111,6 +115,21 @@ public class GlobalDriver {
                     delegate = new EdgeDriver(setEdgeOptions());
                 }
                 break;
+
+            case "safari":
+                WebDriverManager.safaridriver().clearResolutionCache().setup();
+
+                if (executionServer.equalsIgnoreCase("remote")) {
+                    Main.main(new String[]{"standalone", "--port", AppConstants.Web.GRIP_HUB_PORT});
+
+                    delegate = WebDriverManager.safaridriver()
+                            .capabilities(setSafariOptions())
+                            .remoteAddress(AppConstants.Web.GRID_HUB_URL)
+                            .create();
+                } else {
+                    delegate = new SafariDriver(setSafariOptions());
+                }
+                break;
         }
 
         _sDriver = SelfHealingDriver.create(delegate);
@@ -197,6 +216,36 @@ public class GlobalDriver {
         options.addArguments("--use-fake-ui-for-media-stream=1");
         if (_headless.equalsIgnoreCase("true"))
             options.setHeadless(true);
+
+        return options;
+    }
+
+    private SafariOptions setSafariOptions() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("download.default_directory", defaultDownloadPath);  //adding download folder preference
+        prefs.put("download.prompt_for_download", "false");  //preferences for download notification
+        prefs.put("profile.default_content_settings.popups", 0);  //preferences for pop-ups
+        prefs.put("settings.language.preferred_languages", "en");  //language preferences
+
+        SafariOptions options = new SafariOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        options.setCapability("safari.cleansession", true);
+        options.setAutomaticInspection(true);
+        options.setAutomaticProfiling(true);
+
+        options.merge(capabilities);
+
+//        options.addArguments("--disable-notifications");
+//        options.addArguments("--test-type");
+//        options.addArguments("ignore-certificate-errors");
+//        options.addArguments("--disable-extensions");
+//        options.addArguments("start-maximized");
+//        options.addArguments("--use-fake-ui-for-media-stream=1");
+//        if (_headless.equalsIgnoreCase("true"))
+//            options.setHeadless(true);
 
         return options;
     }
