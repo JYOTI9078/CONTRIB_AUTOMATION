a668c42b7b45c5fbd11cc6ddbda356a39abaf43c
bisguha <biswajit.guha@capgemini.com>
2022-10-27 16:38:04 +0530

    initialize all variables as final in AppConstants

--- src/main/java/com/sogeti/automation/framework/constants/AppConstants.java
+++ src/main/java/com/sogeti/automation/framework/constants/AppConstants.java
@@ -4,10 +4,10 @@ import com.sogeti.automation.framework.utils.PropertyReader;
 
 public class AppConstants {
 
-    public static String INTERFACE_TYPE = null;
-    public static String AZURE_KEYVAULT_URL;
+    public static final String INTERFACE_TYPE = PropertyReader.getFieldValue("InterfaceType");
+    public static final String AZURE_KEYVAULT_URL = PropertyReader.getFieldValue("AZURE_KEYVAULT_URL");
 
-    static {
+    /*static {
         try {
             INTERFACE_TYPE = PropertyReader.getFieldValue("InterfaceType");
         } catch (Exception e) {
@@ -21,16 +21,17 @@ public class AppConstants {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
     public static class Web {
-        public static String UI_BASE_URL = null;
-        public static String GRID_HUB_URL = null;
-        public static String GRIP_HUB_PORT = null;
-        public static String UI_USERNAME = null;
-        public static String UI_PASSWORD = null;
+        public static final String UI_BASE_URL = PropertyReader.getFieldValue("UI_BASE_URL");
+        public static final String GRIP_HUB_PORT = PropertyReader.getFieldValue("GRID_HUB_PORT");
+        public static final String GRID_HUB_URL = "http://" + PropertyReader.getFieldValue("GRID_HUB_IP")
+                + ":" + GRIP_HUB_PORT + "/wd/hub";
+        public static final String UI_USERNAME = PropertyReader.getFieldValue("UI_USERNAME");
+        public static final String UI_PASSWORD = PropertyReader.getFieldValue("UI_PASSWORD");
 
-        static {
+        /*static {
             try {
                 UI_BASE_URL = PropertyReader.getFieldValue("UI_BASE_URL");
             } catch (Exception e) {
@@ -69,44 +70,44 @@ public class AppConstants {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
+        }*/
 
     }
 
     public static class Api {
-        public static String API_BASE_URL = null;
-        public static String API_USERNAME = null;
-        public static String API_PASSWORD = null;
-        public static String API_GRANT_TYPE = null;
-        public static String API_TOKEN = null;
-        public static String API_SCOPE = null;
-        public static String API_ACCESS_TOKEN_URL = null;
-        public static String API_CLIENT_ID = null;
-        public static String API_CLIENT_SECRET = null;
-        public static String API_REDIRECT_URL = null;
-        public static String API_RESPONSE_TYPE = null;
-        public static String API_AUTHORIZATION_CODE = null;
-        public static String API_CODE_CHALLENGE = null;
-        public static String API_CODE_CHALLENGE_METHOD = null;
+        public static final String API_BASE_URL = PropertyReader.getFieldValue("API_BASE_URL");
+        public static final String API_USERNAME = PropertyReader.getFieldValue("API_USERNAME");
+        public static final String API_PASSWORD = PropertyReader.getFieldValue("API_PASSWORD");
+        public static final String API_GRANT_TYPE = PropertyReader.getFieldValue("API_GRANT_TYPE");
+        public static final String API_TOKEN = PropertyReader.getFieldValue("API_TOKEN");
+        public static final String API_SCOPE = PropertyReader.getFieldValue("API_SCOPE");
+        public static final String API_ACCESS_TOKEN_URL = PropertyReader.getFieldValue("API_ACCESS_TOKEN_URL");
+        public static final String API_CLIENT_ID = PropertyReader.getFieldValue("API_CLIENT_ID");
+        public static final String API_CLIENT_SECRET = PropertyReader.getFieldValue("API_CLIENT_SECRET");
+        public static final String API_REDIRECT_URL = PropertyReader.getFieldValue("API_REDIRECT_URL");
+        public static final String API_RESPONSE_TYPE = PropertyReader.getFieldValue("API_RESPONSE_TYPE");
+        public static final String API_AUTHORIZATION_CODE = PropertyReader.getFieldValue("API_AUTHORIZATION_CODE");
+        public static final String API_CODE_CHALLENGE = PropertyReader.getFieldValue("API_CODE_CHALLENGE");
+        public static final String API_CODE_CHALLENGE_METHOD = PropertyReader.getFieldValue("API_CODE_CHALLENGE_METHOD");
 
-        public static String GITHUB_USER_URL = "/users/";
-        public static String GITHUB_USER_REPOS = "/user/repos";
-        public static String GITHUB_REPOS = "/repos/";
+        public static final String GITHUB_USER_URL = "/users/";
+        public static final String GITHUB_USER_REPOS = "/user/repos";
+        public static final String GITHUB_REPOS = "/repos/";
 
         public enum AuthenticationType {
-            Basic,
-            Digest,
-            Token,
-            OAuth2
+            BASIC,
+            DIGEST,
+            TOKEN,
+            OAUTH2
         }
 
         public enum GrantType {
-            Authorization_Code,
-            Client_Credentials,
-            Password
+            AUTHORIZATION_CODE,
+            CLIENT_CREDENTIALS,
+            PASSWORD
         }
 
-        static {
+        /*static {
             try {
                 API_BASE_URL = PropertyReader.getFieldValue("API_BASE_URL");
             } catch (Exception e) {
@@ -216,7 +217,7 @@ public class AppConstants {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
+        }*/
     }
 
 }
