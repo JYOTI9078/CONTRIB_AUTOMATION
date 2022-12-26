65a719a3a6773084cd5dab4f115f9879d1513508
bisguha <biswajit.guha@capgemini.com>
2022-12-26 18:58:28 +0530

    added utility methods in WebUtils class

--- src/main/java/com/sogeti/automation/framework/utils/WebUtils.java
+++ src/main/java/com/sogeti/automation/framework/utils/WebUtils.java
@@ -6,9 +6,13 @@ import io.cucumber.java.Scenario;
 import org.apache.logging.log4j.ThreadContext;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WebUtils {
@@ -26,12 +30,20 @@ public class WebUtils {
         PageFactory.initElements(driver, this);
     }
 
-    public void refreshPage() {
+    protected void refreshPage() {
         this._hDriver.navigate().refresh();
     }
 
-    public String getPageTitle() {
+    protected String getPageTitle() {
         return _hDriver.getTitle();
+    }
+
+    protected void scrollIntoView(WebElement element) {
+        try {
+            ((JavascriptExecutor) _hDriver).executeScript("arguments[0].scrollIntoViewIfNeeded();", element);
+        } catch (Exception e) {
+            log.error("Unable to scroll into view of the element\n" + e.getMessage());
+        }
     }
 
     public void takeScreenshot(Scenario name) {
@@ -58,7 +70,7 @@ public class WebUtils {
         }
     }
 
-    public void closeWindow() {
+    protected void closeWindow() {
         try {
             this._hDriver.close();
             log.info("Current window closed successfully.");
@@ -67,7 +79,7 @@ public class WebUtils {
         }
     }
 
-    public void launchUrlInNewTab(String url) {
+    protected void launchUrlInNewTab(String url) {
         try {
             String link = "window.open('" + url + "');";
             ((JavascriptExecutor) _hDriver).executeScript(link);
@@ -79,8 +91,21 @@ public class WebUtils {
         }
     }
 
+    protected void switchToNewTab() {
+        ArrayList<String> tabList = new ArrayList<>(_hDriver.getWindowHandles());
+        _hDriver.switchTo().window(tabList.get(1));
+    }
+
     protected WebElement getWebLocator(String elementToken, String replacement) {
         elementToken = elementToken.replaceAll("\\$\\{.+\\}", replacement);
+        scrollIntoView(_hDriver.findElement(By.xpath(elementToken)));
+
+        return _hDriver.findElement(By.xpath(elementToken));
+    }
+
+    protected WebElement getWebLocator(String elementToken, int index) {
+        elementToken = elementToken.replaceAll("\\$\\{.+\\}", String.valueOf(index));
+        scrollIntoView(_hDriver.findElement(By.xpath(elementToken)));
 
         return _hDriver.findElement(By.xpath(elementToken));
     }
@@ -88,12 +113,22 @@ public class WebUtils {
     protected WebElement getWebLocator(String elementToken, String replacement, int row) {
         elementToken = elementToken.replaceAll("\\$\\{.+\\}", replacement);
         elementToken = elementToken + "[" + row + "]";
+        scrollIntoView(_hDriver.findElement(By.xpath(elementToken)));
+
+        return _hDriver.findElement(By.xpath(elementToken));
+    }
+
+    protected WebElement getWebLocator(String elementToken, String replacement1, String replacement2) {
+        elementToken = elementToken.replaceAll("\\&\\{.+\\}", replacement2);
+        elementToken = elementToken.replaceAll("\\$\\{.+\\}", replacement1);
+        scrollIntoView(_hDriver.findElement(By.xpath(elementToken)));
 
         return _hDriver.findElement(By.xpath(elementToken));
     }
 
     protected List<WebElement> getWebLocatorList(String elementToken, String replacement) {
         elementToken = elementToken.replaceAll("\\$\\{.+\\}", replacement);
+        scrollIntoView(_hDriver.findElement(By.xpath(elementToken)));
 
         return _hDriver.findElements(By.xpath(elementToken));
     }
@@ -101,7 +136,156 @@ public class WebUtils {
     protected List<WebElement> getWebLocatorList(String elementToken, String replacement, int row) {
         elementToken = elementToken.replaceAll("\\$\\{.+\\}", replacement);
         elementToken = elementToken + "[" + row + "]";
+        scrollIntoView(_hDriver.findElement(By.xpath(elementToken)));
 
         return _hDriver.findElements(By.xpath(elementToken));
+    }
+
+    protected WebElement waitForElementToBeClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        scrollIntoView(element);
+
+        return element;
+    }
+
+    protected WebElement waitForElementToBeVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        scrollIntoView(element);
+        return element;
+    }
+
+    protected List<WebElement> waitForElementsToBeVisible(List<WebElement> elements) {
+        return (List<WebElement>) wait.until(ExpectedConditions.visibilityOfAllElements(elements));
+    }
+
+    protected boolean waitForElementToBeInvisible(WebElement element) {
+        return wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    protected boolean waitForElementToBeInvisible(By locator) {
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    protected boolean isElementVisible(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element)) != null;
+    }
+
+    protected boolean isElementVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)) != null;
+    }
+
+    protected void selectByText(WebElement element, String text) {
+        try {
+            Select select = new Select(element);
+            select.selectByVisibleText(text);
+        } catch (NoSuchElementException ne) {
+            log.error("Element not found\n" + ne.getMessage());
+        } catch (Exception e) {
+            log.error("Error in selecting value from dropdown of webelement\n" + e.getMessage());
+        }
+    }
+
+    protected void selectByIndex(WebElement element, int index) {
+        try {
+            Select select = new Select(element);
+            select.selectByIndex(index);
+        } catch (IndexOutOfBoundsException ie) {
+            log.error("Index is out of bounds\n" + ie.getMessage());
+        } catch (NoSuchElementException ne) {
+            log.error("Element not found\n" + ne.getMessage());
+        } catch (Exception e) {
+            log.error("Error in selecting value from dropdown of webelement\n" + e.getMessage());
+        }
+    }
+
+    protected void selectByValue(WebElement element, String value) {
+        try {
+            Select select = new Select(element);
+            select.selectByValue(value);
+        } catch (NoSuchElementException ne) {
+            log.error("Element not found\n" + ne.getMessage());
+        } catch (Exception e) {
+            log.error("Error in selecting value from dropdown of webelement\n" + e.getMessage());
+        }
+    }
+
+    protected void switchToFrame(String frameIdOrName) {
+        FluentWait<SelfHealingDriver> fluentWait = new FluentWait<>(_hDriver);
+        try {
+            fluentWait.withTimeout(Duration.ofSeconds(FrameworkConstants.MEDIUM_WAIT))
+                    .pollingEvery(Duration.ofSeconds(FrameworkConstants.MINIMUM_WAIT))
+                    .ignoring(NoSuchElementException.class)
+                    .until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameIdOrName));
+        } catch (Exception e) {
+            log.error("Unable to switch to frame\n" + e.getMessage());
+        }
+    }
+
+    protected void switchToFrame(int frameIndex) {
+        FluentWait<SelfHealingDriver> fluentWait = new FluentWait<>(_hDriver);
+        try {
+            fluentWait.withTimeout(Duration.ofSeconds(FrameworkConstants.MEDIUM_WAIT))
+                    .pollingEvery(Duration.ofSeconds(FrameworkConstants.MINIMUM_WAIT))
+                    .ignoring(NoSuchElementException.class)
+                    .until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameIndex));
+        } catch (Exception e) {
+            log.error("Unable to switch to frame\n" + e.getMessage());
+        }
+
+    }
+
+    protected void switchToDefaultContent() {
+        try {
+            _hDriver.switchTo().defaultContent();
+        } catch (Exception e) {
+            log.error("Unable to switch to default content\n" + e.getMessage());
+        }
+    }
+
+    protected void switchToParentWindow(String parentHandle) {
+        try {
+            _hDriver.switchTo().window(parentHandle);
+        } catch (Exception e) {
+            log.error("Unable to switch to parent window\n" + e.getMessage());
+        }
+    }
+
+    protected void switchToChildWindow() {
+        try {
+            String parentHandle = _hDriver.getWindowHandle();
+
+            for (String winHandle : _hDriver.getWindowHandles()) {
+                _hDriver.switchTo().window(winHandle);
+            }
+        } catch (Exception e) {
+            log.error("Unable to switch to child window\n" + e.getMessage());
+        }
+    }
+
+    protected void closeAllChildWindows(String parentHandle) {
+        _hDriver.switchTo().window(parentHandle);
+        try {
+            for (String winHandle : _hDriver.getWindowHandles())
+                _hDriver.switchTo().window(winHandle).close();
+        } catch (Exception e) {
+            log.error("Unable to close child window\n" + e.getMessage());
+        }
+    }
+
+    protected boolean waitForChildElementToBeVisible(WebElement element, By childLocator) {
+        boolean status = false;
+        FluentWait<SelfHealingDriver> fluentWait = new FluentWait<>(_hDriver);
+
+        try {
+            fluentWait.withTimeout(Duration.ofSeconds(FrameworkConstants.MEDIUM_WAIT))
+                    .pollingEvery(Duration.ofSeconds(FrameworkConstants.MINIMUM_WAIT))
+                    .ignoring(NoSuchElementException.class)
+                    .until(ExpectedConditions.visibilityOfNestedElementsLocatedBy(element, childLocator));
+            status = true;
+        } catch (Exception e) {
+            log.error("Failed to locate element\n" + e.getMessage());
+        }
+
+        return status;
     }
 }
